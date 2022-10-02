04642984241563721357a23691a5e4853e25dcc1
denizburcu <denizburcua@gmail.com>
2022-10-02 13:48:46 +0300

    String practices

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       String str = "airplane";
+        System.out.println(str.charAt(2));
+        System.out.println(str.charAt(str.length()-1));
+
+        String str1 = "book";
+        System.out.println(str1.concat(" outhor"));
+    }
+
+}
